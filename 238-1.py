c1837b6ff0bc035f034e884062b5be92e6f2741c
Lucas McClean <lucksdeveloper@gmail.com>
2026-01-07 08:57:39 +0000

    238-1.py

---
+++ 238-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = [1] * n
+
+        # Calculate left product
+        product = 1
+        for i in range(n):
+            res[i] *= product
+            product *= nums[i]
+
+        # Calculate right product
+        product = 1
+        for i in range(n-1, -1, -1):
+            res[i] *= product
+            product *= nums[i]
+
+        return res
