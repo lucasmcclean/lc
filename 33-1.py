a747f462941cafe659adc0d265b23e5416e49a01
Lucas McClean <lucksdeveloper@gmail.com>
2026-01-14 11:17:44 +0000

    33-1.py

---
+++ 33-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = l + (r - l) // 2
+            if nums[mid] < target:
+                if nums[r] >= target or nums[r] < nums[mid]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            elif nums[mid] > target:
+                if nums[l] <= target or nums[l] > nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                return mid
+        return -1
