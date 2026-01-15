ba0561fdb65cf95e5416d94d20dae6a649dc7b4c
Lucas McClean <lucksdeveloper@gmail.com>
2026-01-15 13:40:09 +0000

    121-1.py

---
+++ 121-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        max_profit = 0
+        while r < len(prices):
+            if prices[r] <= prices[l]:
+                l = r
+            else:
+                max_profit = max(max_profit, prices[r] - prices[l])
+            r += 1
+        return max_profit
