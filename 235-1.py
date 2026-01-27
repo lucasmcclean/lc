d520799c0a79783349ea7212a99e10a09b53e912
Lucas McClean <lucksdeveloper@gmail.com>
2026-01-27 11:18:48 +0000

    235-[1-2].py

---
+++ 235-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return None
+        if root == p or root == q:
+            return root
+
+        l = self.lowestCommonAncestor(root.left, p, q)
+        r = self.lowestCommonAncestor(root.right, p, q)
+
+        if l and r:
+            return root
+        if l:
+            return l
+        return r
