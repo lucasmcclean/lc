0cd4158f61c951a325e665288b050495a0a931a2
Lucas McClean <lucksdeveloper@gmail.com>
2026-02-02 11:40:32 +0000

    973-1.py

---
+++ 973-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        heap = []
+        for point in points:
+            dist = (point[0]**2 + point[1]**2)**(1/2)
+            if len(heap) == k:
+                heapq.heappushpop_max(heap, (dist, point))
+            else:
+                heapq.heappush_max(heap, (dist, point))
+
+        return [p[1] for p in heap]
