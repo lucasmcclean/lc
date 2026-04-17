405074eadee10b3efdeb2d34b17f54068a203030
Lucas McClean <lucksdeveloper@gmail.com>
2026-04-17 07:37:13 -0400

    143-3.py

---
+++ linked_list/143-3.py
@@ -0,0 +1,30 @@
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+
+        if not head or not head.next:
+            return
+
+        fast, slow = head, head
+        while fast.next and fast.next.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        second = slow.next
+        slow.next = None
+
+        prev = None
+        while second:
+            temp = second.next
+            second.next = prev
+            prev = second
+            second = temp
+
+        l1, l2 = head, prev
+        while l2:
+            t1, t2 = l1.next, l2.next
+            l1.next = l2
+            l2.next = t1
+            l1, l2 = t1, t2
